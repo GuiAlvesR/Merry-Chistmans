3ea024c4966bdb484145fc3438a99f2646f6c823
GuiAlvesR <guinegocios20@gmail.com>
2024-09-20 23:38:43 -0300

    Add repository

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let count = 0;
+const countElement = document.getElementById('cart-count');
+
+const audioMerrychhistmas = new Audio("../assets/audio/merry-christmas.mp3")
+const audioConfetti = new Audio("../assets/audio/confetti.wav")
+
+audioMerrychhistmas.volume = 0.4
+audioConfetti.volume = 0.2
+
+function addItem() {
+    if (count === 0) {
+        countElement.style.display = 'block'
+    }
+
+    count++
+    countElement.innerText = count
+
+    audioMerrychhistmas.play()
+    audioConfetti.play()
+
+    handleConfettiEffect()
+}
